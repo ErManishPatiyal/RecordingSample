59f5966d61d7796f4c436c50168e3430cb8e6694
Manish Patiyal <manish.patiyal@vtnetzwelt.com>
2023-11-04 14:25:09 +0530

    2 minute recording with start stop feature and resets in background

--- app/src/main/java/com/human_factors/notetaker/MainActivity.kt
+++ app/src/main/java/com/human_factors/notetaker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +15,9 @@ import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var countdownTimer: CountDownTimer
     private lateinit var stopTimer: Button
+    private lateinit var playRecording: Button
     private lateinit var countdownTimerText: TextView
     private var mediaRecorder: MediaRecorder? = null
     private var mediaPlayer: MediaPlayer? = null
@@ -26,10 +29,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         countdownTimerText = findViewById(R.id.countdown_timer_text)
-
+        playRecording=findViewById(R.id.play_recording)
         stopTimer = findViewById(R.id.stop_timer)
 
-        val countdownTimer = object : CountDownTimer(120000, 1000) {
+         countdownTimer = object : CountDownTimer(120000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
                 val secondsRemaining = millisUntilFinished / 1000
@@ -39,20 +42,41 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 stopRecording()
                 stopTimer.text = getString(R.string.start)
+                playRecording.visibility= View.VISIBLE
             }
         }
 
-        countdownTimer.start()
-        startRecordingActual()
+        playRecording.setOnClickListener {
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer().apply {
+                    setDataSource(currentAudioPath)
+                    prepare()
+                    start()
+                    playRecording.text = getString(R.string.stop)
+                }
+                mediaPlayer?.setOnCompletionListener {
+                    it.release()
+                    mediaPlayer = null
+                    playRecording.text = getString(R.string.play)
+                }
+            } else {
+                mediaPlayer?.stop()
+                mediaPlayer?.release()
+                mediaPlayer = null
+                playRecording.text = getString(R.string.play)
+            }
+        }
         stopTimer.setOnClickListener {
             if (isRecording) {
                 countdownTimer.cancel()
                 stopRecording()
                 stopTimer.text = getString(R.string.start)
+                playRecording.visibility= View.VISIBLE
             } else {
                 countdownTimer.start()
                 startRecording()
                 stopTimer.text = getString(R.string.stop)
+                playRecording.visibility= View.GONE
             }
         }
 
@@ -109,6 +133,27 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == RECORD_AUDIO_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startRecording()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        countdownTimerText.text=""
+        countdownTimer.start()
+        startRecordingActual()
+        stopTimer.text= getString(R.string.stop)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        countdownTimer.cancel()
+        playRecording.visibility=View.GONE
+        mediaPlayer?.release()
+        mediaPlayer = null
+        if (isRecording) {
+            mediaRecorder?.release()
+            mediaRecorder = null
         }
     }
 
